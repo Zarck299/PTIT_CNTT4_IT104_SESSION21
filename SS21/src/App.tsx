3f69c998b7b12aca55a659c0b387a4a3412d4bc2
Anh <honganhzarck@gmail.com>
2025-09-03 23:43:40 +0700

    SS21

--- SS21/src/App.tsx
+++ SS21/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 // import ChangeColor from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE01/Bai1'
 // import Square from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE02/Bai2'
 // import Circle from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE03/Bai3'
-import ButtonHover from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE04/Bai4'
+// import ButtonHover from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE04/Bai4'
 // import Position from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE05/Bai5'
 // import OverflowBox from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE06/Bai6'
 //  import GridBoxes from './component/PTIT_CNTT4_IT104_SESSION21_EXERCISE07/Bai7'
@@ -13,7 +13,7 @@ export default function App() {
         {/* <ChangeColor></ChangeColor> */}
         {/* <Square></Square> */}
         {/* <Circle></Circle> */}
-        <ButtonHover></ButtonHover>
+        {/* <ButtonHover></ButtonHover> */}
         {/* <Position></Position> */}
         {/* <OverflowBox></OverflowBox> */}
         {/* <GridBoxes></GridBoxes> */}
